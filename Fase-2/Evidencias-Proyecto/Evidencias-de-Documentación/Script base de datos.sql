0456dd8ecb8a7dfac731dcf50f5bcbb52c22e486
eixaufnex <ianfrancovargas@gmail.com>
2024-10-08 22:15:16 -0300

    Modelado de datos v2.

--- Fase-2/Evidencias-Proyecto/Evidencias-de-Documentación/Script base de datos.sql
+++ Fase-2/Evidencias-Proyecto/Evidencias-de-Documentación/Script base de datos.sql
@@ -1,80 +1,70 @@
-
-
 CREATE TABLE USUARIO (
-    id_usuario INT PRIMARY KEY NOT NULL,
-    fecha_registro DATE NOT NULL
+    id_usuario INT PRIMARY KEY NOT NULL, --PK
+    username VARCHAR(50) NOT NULL,
+	rol VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+	fecha_registro DATE NOT NULL
+
 );
-CREATE TABLE ESTADO(
-	id_estado INT PRIMARY KEY NOT NULL,
-	nombre_estado VARCHAR(20)
-);
+
 
 CREATE TABLE CUENTA (
-	id_cuenta INT PRIMARY KEY NOT NULL,
-	id_estado INT NOT NULL,
-	id_usuario INT NOT NULL,
+	id_cuenta INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
 	email VARCHAR(50) UNIQUE NOT NULL,
 	user_password VARCHAR(50) NOT NULL,  
-	FOREIGN KEY (id_estado) REFERENCES ESTADO(id_estado) ON DELETE CASCADE,
 	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
 );
 
 
 CREATE TABLE PERFIL(
-	id_perfil INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
-	username VARCHAR(50) NOT NULL,
+	id_perfil INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
 	nombre VARCHAR(50) NOT NULL,
 	apellido VARCHAR(50) NOT NULL,
+    tipo_nivel VARCHAR(50) NOT NULL,
 	foto_perfil IMAGE NULL,
 	biografia VARCHAR(100) NULL,
-	nivel VARCHAR(20) NULL,	
 	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
 );
 
-
-CREATE TABLE ROL(
-	id_rol INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
-	nombre_rol VARCHAR(20) NOT NULL,
-	descripcion_rol VARCHAR(40) NOT NULL,
-	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
-);
 
 CREATE TABLE FEEDBACK(
-	id_feedback INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
+	id_feedback INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
+    tipo_feedback VARCHAR(50) NOT NULL,
 	contenido_feed VARCHAR(200) NOT NULL,
 	fecha_envio_feed DATE NOT NULL,
-	tipo_feed VARCHAR(50),
 	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
 );
 
 
 CREATE TABLE NOTIFICACION(
-	id_notificacion INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
-	tipo_notificacion VARCHAR(30) NOT NULL,
+	id_notificacion INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
+	tipo_notificacion VARCHAR(50) NOT NULL,
 	mensaje_notificacion VARCHAR(100) NOT NULL,
 	fecha_notificacion DATE NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
 );
 
+
 CREATE TABLE OBJETIVO(
-	id_objetivo INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
+	id_objetivo INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
+    tipo_objetivo VARCHAR(50) NOT NULL,
 	nombre_objetivo VARCHAR(50) NOT NULL,
 	descripcion_objetivo VARCHAR(200) NOT NULL,
-	tipo_objetivo VARCHAR(50) NOT NULL,
 	valor_objetivo INT NULL,
 	fecha_limite DATE NOT NULL,
 	alcanzado VARCHAR(10) NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
 );
 
+
 CREATE TABLE LOGRO(
-	id_logro INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
+	id_logro INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
 	nombre_logro VARCHAR(50) NOT NULL,
 	descripcion_logro VARCHAR(200) NOT NULL,
 	fecha_obtencion DATE NOT NULL,
@@ -82,22 +72,40 @@ CREATE TABLE LOGRO(
 );
 
 
-
 CREATE TABLE RUTINA(
-	id_rutina INT PRIMARY KEY NOT NULL,
-	id_usuario INT NOT NULL,
+	id_rutina INT PRIMARY KEY NOT NULL, --PK
+	id_usuario INT NOT NULL, --FK
 	nombre_rutina VARCHAR(50) NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario) ON DELETE CASCADE
 );
 
+
+CREATE TABLE GRUPO_MUSCULAR(
+	id_grupo_muscular INT PRIMARY KEY NOT NULL, --PK
+	grupo_musculo VARCHAR(50) NOT NULL,
+	descripcion_musculo VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE LISTA_EJERCICIO(
+	id_lista_ejercicio INT PRIMARY KEY NOT NULL, --PK
+	id_grupo_muscular INT NOT NULL, --FK
+	nombre_ejercicio VARCHAR(50) NOT NULL,
+	imagen_ejercicio IMAGE NULL,
+	FOREIGN KEY (id_grupo_muscular) REFERENCES GRUPO_MUSCULAR(id_grupo_muscular) ON DELETE CASCADE
+);
+
+
 CREATE TABLE RUTINA_DETALLADA(
-	id_rutina_detallada INT PRIMARY KEY NOT NULL,
-	id_rutina INT NOT NULL,
+	id_rutina_detallada INT PRIMARY KEY NOT NULL, --PK
+	id_lista_ejercicio INT NOT NULL, --FK
+	id_rutina INT NOT NULL, --FK
 	series INT NOT NULL,
 	repeticiones INT NOT NULL,
 	peso FLOAT NOT NULL,
 	descanso INT NULL,
 	fecha_rutina DATE NOT NULL, 
+	FOREIGN KEY (id_lista_ejercicio) REFERENCES LISTA_EJERCICIO(id_lista_ejercicio) ON DELETE CASCADE,
 	FOREIGN KEY (id_rutina) REFERENCES RUTINA(id_rutina) ON DELETE CASCADE
 );
 
@@ -109,22 +117,6 @@ CREATE TABLE PROGRESO(
 	FOREIGN KEY (id_rutina_detallada) REFERENCES RUTINA_DETALLADA(id_rutina_detallada) ON DELETE CASCADE
 );
 
-CREATE TABLE CATEGORIA(
-	id_categoria INT PRIMARY KEY NOT NULL,
-	id_rutina_detallada INT NOT NULL,
-	grupo_musculo VARCHAR(50) NOT NULL,
-	tipo_ejercicio VARCHAR(50) NOT NULL,
-	FOREIGN KEY (id_rutina_detallada) REFERENCES RUTINA_DETALLADA(id_rutina_detallada) ON DELETE CASCADE
-);
-
-
-CREATE TABLE LISTA_EJERCICIO(
-	id_lista_ejercicio INT PRIMARY KEY NOT NULL,
-	id_categoria INT NOT NULL,
-	nombre_ejercicio VARCHAR(50) NOT NULL,
-	imagen_ejercicio IMAGE NULL,
-	FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id_categoria) ON DELETE CASCADE
-);
 
 CREATE TABLE DETALLE_EJERCICIO(
 	id_detalle_ejercicio INT PRIMARY KEY NOT NULL,
